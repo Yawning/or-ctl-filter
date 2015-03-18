fee2ccc2713d6cc95e191835739bbc243cecbd30
Yawning Angel <yawning@schwanenlied.me>
2015-03-18 17:46:20 +0000

    Refactor to use bulb, amongst other things.
     * Now uses bulb to interact with the tor control port.
     * Now supports all valid commands in the pre-AUTHENTICATE state.
     * Logging output is nicer.
     * Various code cleanups.

--- main.go
+++ main.go
@@ -17,19 +17,16 @@ package main
 
 import (
 	"bufio"
-	"crypto/hmac"
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/hex"
+	"bytes"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 	"sync"
+
+	"github.com/yawning/bulb"
 )
 
 const (
@@ -38,227 +35,33 @@ const (
 	controlSocketFile = "/var/run/tor/control"
 	torControlAddr    = "127.0.0.1:9151" // Match ControlPort in torrc-defaults.
 
-	cmdProtocolInfo  = "PROTOCOLINFO"
-	cmdAuthenticate  = "AUTHENTICATE"
-	cmdAuthChallenge = "AUTHCHALLENGE"
-	cmdGetInfo       = "GETINFO"
-	cmdSignal        = "SIGNAL"
+	cmdGetInfo = "GETINFO"
+	cmdSignal  = "SIGNAL"
 
-	argSignalNewnym = "NEWNYM"
-	argGetinfoSocks = "net/listeners/socks"
-	argServerHash   = "SERVERHASH="
-	argServerNonce  = "SERVERNONCE="
-
-	respProtocolInfoAuth       = "250-AUTH"
-	respProtocolInfoMethods    = "METHODS="
-	respProtocolInfoCookieFile = "COOKIEFILE="
-
-	respAuthChallenge = "250 AUTHCHALLENGE "
-
-	authMethodNull       = "NULL"
-	authMethodCookie     = "COOKIE"
-	authMethodSafeCookie = "SAFECOOKIE"
-
-	authNonceLength   = 32
-	authServerHashKey = "Tor safe cookie authentication server-to-controller hash"
-	authClientHashKey = "Tor safe cookie authentication controller-to-server hash"
-
-	errAuthenticationRequired = "514 Authentication required\n"
-	errUnrecognizedCommand    = "510 Unrecognized command\n"
+	errUnrecognizedCommand = "510 Unrecognized command\n"
 )
 
-var filteredControlAddr *net.UnixAddr
-var enableLogging bool
-var logFile string
-
-func readAuthCookie(path string) ([]byte, error) {
-	// Read the cookie auth file.
-	cookie, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("reading cookie auth file: %s", err)
-	}
-	return cookie, nil
-}
-
-func authSafeCookie(conn net.Conn, connReader *bufio.Reader, cookie []byte) ([]byte, error) {
-	clientNonce := make([]byte, authNonceLength)
-	if _, err := rand.Read(clientNonce); err != nil {
-		return nil, fmt.Errorf("generating AUTHCHALLENGE nonce: %s", err)
-	}
-	clientNonceStr := hex.EncodeToString(clientNonce)
-
-	// Send and process the AUTHCHALLENGE.
-	authChallengeReq := []byte(fmt.Sprintf("%s %s %s\n", cmdAuthChallenge, authMethodSafeCookie, clientNonceStr))
-	if _, err := conn.Write(authChallengeReq); err != nil {
-		return nil, fmt.Errorf("writing AUTHCHALLENGE request: %s", err)
-	}
-	line, err := connReader.ReadBytes('\n')
-	if err != nil {
-		return nil, fmt.Errorf("reading AUTHCHALLENGE response: %s", err)
-	}
-	lineStr := strings.TrimSpace(string(line))
-	respStr := strings.TrimPrefix(lineStr, respAuthChallenge)
-	if respStr == lineStr {
-		return nil, fmt.Errorf("parsing AUTHCHALLENGE response")
-	}
-	splitResp := strings.SplitN(respStr, " ", 2)
-	if len(splitResp) != 2 {
-		return nil, fmt.Errorf("parsing AUTHCHALLENGE response")
-	}
-	hashStr := strings.TrimPrefix(splitResp[0], argServerHash)
-	serverHash, err := hex.DecodeString(hashStr)
-	if err != nil {
-		return nil, fmt.Errorf("decoding AUTHCHALLENGE ServerHash: %s", err)
-	}
-	serverNonceStr := strings.TrimPrefix(splitResp[1], argServerNonce)
-	serverNonce, err := hex.DecodeString(serverNonceStr)
-	if err != nil {
-		return nil, fmt.Errorf("decoding AUTHCHALLENGE ServerNonce: %s", err)
-	}
-
-	// Validate the ServerHash.
-	m := hmac.New(sha256.New, []byte(authServerHashKey))
-	m.Write([]byte(cookie))
-	m.Write([]byte(clientNonce))
-	m.Write([]byte(serverNonce))
-	dervServerHash := m.Sum(nil)
-	if !hmac.Equal(serverHash, dervServerHash) {
-		return nil, fmt.Errorf("AUTHCHALLENGE ServerHash is invalid")
-	}
-
-	// Calculate the ClientHash.
-	m = hmac.New(sha256.New, []byte(authClientHashKey))
-	m.Write([]byte(cookie))
-	m.Write([]byte(clientNonce))
-	m.Write([]byte(serverNonce))
-
-	return m.Sum(nil), nil
-}
-
-func authenticate(torConn net.Conn, torConnReader *bufio.Reader, appConn net.Conn, appConnReader *bufio.Reader) error {
-	var canNull, canCookie, canSafeCookie bool
-	var cookiePath string
-
-	// Figure out the best auth method, and where the cookie is if any.
-	protocolInfoReq := []byte(fmt.Sprintf("%s\n", cmdProtocolInfo))
-	if _, err := torConn.Write(protocolInfoReq); err != nil {
-		return fmt.Errorf("writing PROTOCOLINFO request: %s", err)
-	}
-	for {
-		line, err := torConnReader.ReadBytes('\n')
-		if err != nil {
-			return fmt.Errorf("reading PROTOCOLINFO response: %s", err)
-		}
-		lineStr := strings.TrimSpace(string(line))
-		if !strings.HasPrefix(lineStr, "250") {
-			return fmt.Errorf("parsing PROTOCOLINFO response")
-		} else if lineStr == "250 OK" {
-			break
-		}
-		splitResp := strings.SplitN(lineStr, " ", 3)
-		if splitResp[0] == respProtocolInfoAuth {
-			if len(splitResp) == 1 {
-				continue
-			}
-
-			methodsStr := strings.TrimPrefix(splitResp[1], respProtocolInfoMethods)
-			if methodsStr == splitResp[1] {
-				continue
-			}
-			methods := strings.Split(methodsStr, ",")
-			for _, method := range methods {
-				switch method {
-				case authMethodNull:
-					canNull = true
-				case authMethodCookie:
-					canCookie = true
-				case authMethodSafeCookie:
-					canSafeCookie = true
-				}
-			}
-			if (canCookie || canSafeCookie) && len(splitResp) == 3 {
-				cookiePathStr := strings.TrimPrefix(splitResp[2], respProtocolInfoCookieFile)
-				if cookiePathStr == splitResp[2] {
-					continue
-				}
-				cookiePath, err = strconv.Unquote(cookiePathStr)
-				if err != nil {
-					continue
-				}
-			}
-		}
-	}
-
-	// Authenticate using the best possible authentication method.
-	var authReq []byte
-	if canNull {
-		authReq = []byte(fmt.Sprintf("%s\n", cmdAuthenticate))
-	} else if (canCookie || canSafeCookie) && (cookiePath != "") {
-		// Read the auth cookie.
-		cookie, err := readAuthCookie(cookiePath)
-		if err != nil {
-			return err
-		}
-		if canSafeCookie {
-			cookie, err = authSafeCookie(torConn, torConnReader, cookie)
-			if err != nil {
-				return err
-			}
-		}
-		cookieStr := hex.EncodeToString(cookie)
-		authReq = []byte(fmt.Sprintf("%s %s\n", cmdAuthenticate, cookieStr))
-	} else {
-		return fmt.Errorf("no supported authentication methods")
-	}
-	if _, err := torConn.Write(authReq); err != nil {
-		return fmt.Errorf("writing AUTHENTICATE request: %s", err)
-	}
-	authResp, err := torConnReader.ReadBytes('\n')
-	if err != nil {
-		return fmt.Errorf("reading AUTHENTICATE response: %s", err)
-	}
-
-	// "Authenticate" the application.
-	authReq, err = appConnReader.ReadBytes('\n')
-	if err != nil {
-		return fmt.Errorf("reading app AUTHENTICATE request:%s", err)
-	}
-	splitReq := strings.SplitN(string(authReq), " ", 2)
-	if strings.ToUpper(splitReq[0]) != cmdAuthenticate { // TODO: PROTOCOLINFO/AUTHCHALLENGE/QUIT?
-		appConn.Write([]byte(errAuthenticationRequired))
-		return fmt.Errorf("invalid app command: '%s'", splitReq[0])
-	}
-	if _, err = appConn.Write(authResp); err != nil {
-		return fmt.Errorf("writing app AUTHENTICATE response: %s", err)
-	}
-	return nil
-}
-
-func syncedWrite(l *sync.Mutex, conn net.Conn, buf []byte) (int, error) {
-	l.Lock()
-	defer l.Unlock()
-	return conn.Write(buf)
-}
-
 func validateCmdSignal(splitReq []string) bool {
+	const argSignalNewnym = "NEWNYM"
 	if len(splitReq) != 2 {
-		log.Printf("A->T: Filtering SIGNAL with invalid args\n")
+		log.Printf("Filtering SIGNAL with invalid args\n")
 		return false
 	}
 	if splitReq[1] != argSignalNewnym {
-		log.Printf("A->T: Filtering SIGNAL: [%s]\n", splitReq[1])
+		log.Printf("Filtering SIGNAL: [%s]\n", splitReq[1])
 		return false
 	}
 	return true
 }
 
 func validateCmdGetinfo(splitReq []string) bool {
+	const argGetinfoSocks = "net/listeners/socks"
 	if len(splitReq) != 2 {
-		log.Printf("A->T: Filtering GETINFO with unexpected args\n")
+		log.Printf("Filtering GETINFO with unexpected args\n")
 		return false
 	}
 	if splitReq[1] != argGetinfoSocks {
-		log.Printf("A->T: Filtering GETINFO: [%s]\n", splitReq[1])
+		log.Printf("Filtering GETINFO: [%s]\n", splitReq[1])
 		return false
 	}
 	return true
@@ -267,38 +70,108 @@ func validateCmdGetinfo(splitReq []string) bool {
 func filterConnection(appConn net.Conn) {
 	defer appConn.Close()
 
+	appConnReader := bufio.NewReader(appConn)
 	clientAddr := appConn.RemoteAddr()
 	log.Printf("New app connection from: %s\n", clientAddr)
 
-	torConn, err := net.DialUnix("unix", nil, filteredControlAddr)
+	torConn, err := bulb.Dial("unix", controlSocketFile)
 	if err != nil {
 		log.Printf("Failed to connect to the tor control port: %s\n", err)
 		return
 	}
 	defer torConn.Close()
 
-	// Authenticate with the real control port, and wait for the application to
-	// authenticate.
-	torConnReader := bufio.NewReader(torConn)
-	appConnReader := bufio.NewReader(appConn)
-	if err = authenticate(torConn, torConnReader, appConn, appConnReader); err != nil {
-		log.Printf("Failed to authenticate: %s\n", err)
+	// Authenticate with the real tor connection.
+	if err = torConn.Authenticate(); err != nil {
+		log.Printf("Failed to connect to the tor control port: %s\n", err)
 		return
 	}
 
-	// Start filtering commands as appropriate.
+	// Get a valid PROTOCOLINFO command so we can lie about the version.
+	pi, err := torConn.ProtocolInfo()
+	if err != nil {
+		log.Printf("Failed to query protocol info: %s\n", err)
+		return
+	}
+
+	const (
+		proxyToClientPreAuth = iota
+		proxyToClient
+		serverToClient
+	)
+	var appConnLock sync.Mutex
+	writeAppConn := func(direction int, b []byte) (int, error) {
+		var prefix string
+		switch direction {
+		case proxyToClientPreAuth:
+			prefix = "P->C [PreAuth]:"
+		case proxyToClient:
+			prefix = "P->C:"
+		case serverToClient:
+			prefix = "S->C:"
+		}
+		appConnLock.Lock()
+		defer appConnLock.Unlock()
+		log.Printf("%s %s", prefix, bytes.TrimSpace(b))
+		return appConn.Write(b)
+	}
+
+	// Ok.  It looks like we can talk to the real control port.
+	// At this point the application has yet to authenticate so it
+	// can either "PROTOCOLINFO"/"QUIT" or attempt to authenticate via
+	// "AUTHCHALLENGE"/"AUTHENTICATE".
+preauthLoop:
+	for {
+		// Fake results entirely till the client authenticates.
+		const (
+			cmdProtocolInfo  = "PROTOCOLINFO"
+			cmdAuthenticate  = "AUTHENTICATE"
+			cmdAuthChallenge = "AUTHCHALLENGE"
+			cmdQuit          = "QUIT"
+
+			responseOk                = "250 OK\r\n"
+			errAuthenticationRequired = "514 Authentication required\r\n"
+		)
+
+		appReq, err := appConnReader.ReadBytes('\n')
+		if err != nil {
+			log.Printf("[PreAuth]: Failed reading client request: %s", err)
+			return
+		}
+		log.Printf("C [PreAuth]: %s", bytes.TrimSpace(appReq))
+
+		splitReq := strings.SplitN(string(appReq), " ", 2)
+		switch strings.ToUpper(strings.TrimSpace(splitReq[0])) {
+		case cmdProtocolInfo:
+			respStr := "250-PROTOCOLINFO 1\r\n250-AUTH METHODS=NULL,PASSWORD\r\n250-VERSION Tor=\"" + pi.TorVersion + "\"\r\n" + responseOk
+			writeAppConn(proxyToClientPreAuth, []byte(respStr))
+		case cmdAuthenticate:
+			writeAppConn(proxyToClientPreAuth, []byte(responseOk))
+			break preauthLoop
+		case cmdAuthChallenge:
+			// WTF?  We should never see this since PROTOCOLINFO lies about the
+			// supported authentication types.
+			log.Printf("[PreAuth]: Client sent AUTHCHALLENGE, when not supported")
+			writeAppConn(proxyToClientPreAuth, []byte(errUnrecognizedCommand))
+			return
+		case cmdQuit:
+			log.Printf("[PreAuth]: Client requested connection close")
+			return
+		default:
+			log.Printf("[PreAuth]: Invalid app command: '%s'", splitReq[0])
+			writeAppConn(proxyToClientPreAuth, []byte(errAuthenticationRequired))
+			return
+		}
+	}
+
+	// Initialize the filtering/proxy handlers.
+	//  * Tor->Client chatter: Direct.
+	//  * Client->Tor chatter: Intercepted.
+	torConnReader := bufio.NewReader(torConn)
 	errChan := make(chan error, 2)
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	var appConnLock sync.Mutex
-	writeAppConn := func(b []byte) (int, error) {
-		appConnLock.Lock()
-		defer appConnLock.Unlock()
-		return appConn.Write(b)
-	}
-
-	// Just proxy tor to application chatter.
 	go func() {
 		defer wg.Done()
 		defer appConn.Close()
@@ -310,17 +183,13 @@ func filterConnection(appConn net.Conn) {
 				errChan <- err
 				break
 			}
-			lineStr := strings.TrimSpace(string(line))
-			log.Printf("A<-T: [%s]\n", lineStr)
-
-			if _, err = writeAppConn(line); err != nil {
+			if _, err = writeAppConn(serverToClient, line); err != nil {
 				errChan <- err
 				break
 			}
 		}
 	}()
 
-	// Filter and selectively proxy or deny application to tor chatter.
 	go func() {
 		defer wg.Done()
 		defer torConn.Close()
@@ -333,7 +202,7 @@ func filterConnection(appConn net.Conn) {
 				break
 			}
 			lineStr := strings.TrimSpace(string(line))
-			log.Printf("A->T: [%s]\n", lineStr)
+			log.Printf("C: %s", lineStr)
 
 			// Filter out commands that aren't "required" for Tor Browser to
 			// work with SKIP_LAUNCH etc set.
@@ -346,7 +215,7 @@ func filterConnection(appConn net.Conn) {
 			case cmdSignal:
 				allow = validateCmdSignal(splitReq)
 			default:
-				log.Printf("A->T: Filtering command: [%s]\n", cmd)
+				log.Printf("Filtering command: [%s]\n", cmd)
 			}
 
 			if allow {
@@ -355,7 +224,7 @@ func filterConnection(appConn net.Conn) {
 					break
 				}
 			} else {
-				if _, err = writeAppConn([]byte(errUnrecognizedCommand)); err != nil {
+				if _, err = writeAppConn(proxyToClient, []byte(errUnrecognizedCommand)); err != nil {
 					errChan <- err
 					break
 				}
@@ -373,26 +242,19 @@ func filterConnection(appConn net.Conn) {
 }
 
 func main() {
-	var err error
-
-	flag.BoolVar(&enableLogging, "enable-logging", false, "enable logging")
-	flag.StringVar(&logFile, "log-file", defaultLogFile, "log file")
+	enableLogging := flag.Bool("enable-logging", false, "enable logging")
+	logFile := flag.String("log-file", defaultLogFile, "log file")
 	flag.Parse()
 
 	// Deal with logging.
-	if !enableLogging {
+	if !*enableLogging {
 		log.SetOutput(ioutil.Discard)
-	} else if logFile != "" {
-		f, err := os.OpenFile(logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	} else if *logFile != "" {
+		f, err := os.OpenFile(*logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 		if err != nil {
 			log.Fatalf("Failed to create log file: %s\n", err)
 		}
 		log.SetOutput(f)
-	}
-
-	filteredControlAddr, err = net.ResolveUnixAddr("unix", controlSocketFile)
-	if err != nil {
-		log.Fatalf("Failed to resolve the control port: %s\n", err)
 	}
 
 	// Initialize the listener
